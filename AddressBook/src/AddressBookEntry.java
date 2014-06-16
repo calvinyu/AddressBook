eafb7a8898ef5c06973ae62b6199b317bab94f5e
Calvin Yu <ycy247@nyu.edu>
2014-06-16 14:35:10 -0400

    Initial Commit

---
+++ AddressBook/src/AddressBookEntry.java
@@ -0,0 +1,13 @@
+/**
+ * Each AddressBookEntry contains one record.
+ * @author yuyuchih
+ *
+ */
+public class AddressBookEntry {
+	private String name;
+	private String postalAddress;
+	private String phoneNumber;
+	private String emailAddress;
+	private String note;
+	
+}
